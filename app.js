3ac08f346d50dae8105c53015048ca0d4ff2e54f
Rohit <vr.rohit03@gmail.com>
2021-04-02 13:44:57 +0530

    DB initialization

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -10,6 +11,18 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+// creating DB
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+// creating Schema
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+// creating Model
+const User = mongoose.model("User", userSchema);
 
 app.get('/', function(req, res) {
   res.render("home");
@@ -19,8 +32,40 @@ app.get('/login', function(req, res) {
   res.render("login");
 });
 
+app.post('/login', function(req, res) {
+  const userName = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: userName}, function(err, result){
+    if(err) {
+      console.log(err);
+    } else {
+      if(result) {
+        if(result.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
+
 app.get('/register', function(req, res) {
   res.render("register");
+});
+
+app.post('/register', function(req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
 });
 
 app.listen(3000, function() {
