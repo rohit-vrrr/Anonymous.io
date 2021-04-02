730cd0196ef5ec5ff263e840c4317bd5c7ca9056
Rohit <vr.rohit03@gmail.com>
2021-04-02 14:35:07 +0530

    Level 2 environment variables

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
 
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -22,8 +23,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "Thisisourlittleseecret.";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 // creating Model
 const User = mongoose.model("User", userSchema);
